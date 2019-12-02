bc5ff92589745c61f5fe0cb6459b548e2349b374
Jesse Erwin <jesse@jesseerwin.net>
2019-12-01 18:05:33 -0600

    day 1

---
+++ 1/1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.6
+import random
+import os
+
+lines = [line.rstrip('\n') for line in open('file')]
+ans = 0
+b = 0
+c = 0
+d = 0
+fric = 5
+int(b)
+for a in lines:
+    c = int(a) // 3 - 2 
+    d = c + d
+    print(d)
+    
+print(d)
---
+++ 1/2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.6
+import random
+import os
+global b
+b = 0 
+total = 0
+lines = [line.rstrip('\n') for line in open('file')]
+
+for a in lines:
+    num = a
+    while True:
+        num = (int(num) // 3) - 2
+        if num > 0:
+            total = total + int(num)
+        else:
+            break
+
+print(total)
